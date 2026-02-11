86aec29b87a7a03755e73a79dbd75eaf587c3147
tim-z-donut <200664920+tim-z-donut@users.noreply.github.com>
2026-02-11 16:05:29 -0500

    Basic counter component that clears itself before reload

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,13 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const bacon = "Wendry Baconator"
+
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js {bacon}</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -17,9 +22,38 @@ function App() {
         >
           Learn React
         </a>
+        <Counter></Counter>
       </header>
     </div>
   );
 }
 
 export default App;
+
+
+
+
+function Counter() {
+  // Define a function that updates a constant variable
+  const [count, setCount] =useState(0);
+  // use Effect to clear a value after use to prevent glitching artifacts
+  useEffect(function() {
+    
+    // main logic
+    var timer = setInterval(function() {
+      setCount(count+1);
+      console.log("count =", count);
+
+    }, 1000);
+    // clear timer value before next component update
+    return function() {
+      clearInterval(timer);
+    }
+  })
+  return (
+    <div className="Counter">
+    <h1>Hi there: count is </h1>
+    <p>{count}</p>
+  </div>
+  )
+}
